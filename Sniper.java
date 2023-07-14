4e517650cd5f2dae072b2eb87ee5f1fb6a850b65
Alisa <aesonse@yandex.ru>
2023-07-14 20:02:19 +0300

    HW2 удалены лишние файлы

--- Sniper.java
+++
@@ -1,8 +0,0 @@
-package Семинар;
-/**стрелять                                        дальность,  */
-public class Sniper extends RangeUnit{
-
-    public Sniper() {
-        super(100, 10, 2, 1, 20, 5, 20);
-    } 
-}
--- Spearman.java
+++
@@ -1,8 +0,0 @@
-package Семинар;
-/**стена копий */
-public class Spearman extends BaseUnit{
-    public Spearman() {
-        super(10, 10, 20, 30, 40, 50);
-    }
-    
-}
